5c6218373ce1700bc279eb9f2c7d02c7799e6c33
chuhan2014 <3228936343@qq.com>
2018-03-31 17:30:13 +0800

    心跳包

--- app/src/main/java/com/gapcoder/weico/MessageService/MessageService.java
+++ app/src/main/java/com/gapcoder/weico/MessageService/MessageService.java
@@ -18,8 +18,11 @@ import java.net.Socket;
 
 public class MessageService extends Service {
 
+
     Socket s=null;
     Gson gson=new Gson();
+    int delay=5000;
+    long last=0;
     public MessageService() {
 
     }
@@ -32,22 +35,53 @@ public class MessageService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        init();
+        task();
+        return START_STICKY;
+    }
 
+    private void task(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(true) {
+                        Thread.sleep(3 * delay);
+                        if(System.currentTimeMillis()-last<3*delay){
+                            continue;
+                        }
+                        if(s!=null){
+                            if(!s.isClosed())
+                                s.close();
+                            Thread.sleep(delay);
+                            init();
+                        }
+                    }
+                }catch(Exception e){
+                    Log.i("tag",e.toString());
+                }
+            }
+        }).start();
+
+    }
+    private void init(){
         new Thread(new Runnable() {
             public void run() {
                 try {
+                    Log.i("tag","r1 start");
                     s = new Socket("10.0.2.2", 8888);
                     OutputStream os = s.getOutputStream();
                     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os));
                     while(true){
-                        Log.i("tag","{\"token\":\""+Token.token+"\",\"num\":0}\n");
                         bw.write("{\"token\":\""+Token.token+"\",\"num\":0}\n");
                         bw.flush();
-                        Thread.sleep(5000);
+                        Thread.sleep(delay);
                     }
                 }catch (Exception e){
                     Log.w("tag",e.toString());
                 }
+
+                Log.i("tag","r1 exit");
             }
         }).start();
 
@@ -56,23 +90,26 @@ public class MessageService extends Service {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(5000);
+                    Log.i("tag","r2 start");
+                    Thread.sleep(delay);
                     InputStream is = s.getInputStream();
                     BufferedReader br=new BufferedReader(new InputStreamReader(is));
                     while(true) {
                         String s=br.readLine();
+                        Log.i("tag",s);
                         Message ins=gson.fromJson(s,Message.class);
                         if(ins.getNum()>0) {
                             Intent i = new Intent("com.gapcoder.weico.MESSAGE");
                             i.putExtra("num", ins.getNum());
                             sendBroadcast(i);
                         }
+                        last=System.currentTimeMillis();
                     }
                 }catch(Exception e){
                     Log.w("tag",e.toString());
                 }
+                Log.i("tag","r2 exit");
             }
         }).start();
-        return START_STICKY;
     }
 }
